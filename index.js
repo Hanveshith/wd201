fae2011385bb7dacf5ce448a2da0028f03cbc32d
Hanveshith <110842917+Hanveshith@users.noreply.github.com>
2023-03-27 19:09:30 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-app.get('/registration', (req, res) => {
-    res.sendFile(path.join(__dirname, 'http-server', 'registration.html'));
-  });
-  const port = process.argv[process.argv.indexOf('--port') + 1] || 3000;
-  app.listen(port, () => console.log(`Server listening on port ${port}`));
